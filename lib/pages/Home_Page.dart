d5254f0291092bc1af3e0a0efb10fc309de92c5b
Praveen Mehta <praveen.mehta010@gmail.com>
2022-07-03 09:12:32 +0530

    Grid View control

--- lib/pages/Home_Page.dart
+++ lib/pages/Home_Page.dart
@@ -25,9 +25,8 @@ class _HomePageState extends State<HomePage> {
     var catalogjson = await rootBundle.loadString("assets/files/catalog.json");
     var decodedData = jsonDecode(catalogjson);
     var productData = decodedData["products"];
-    CatalogModles.items = List.from(productData)
-    .map<Item>((item) => Item.fromMap(item))
-    .toList();    
+    CatalogModles.items =
+        List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
     setState(() {});
   }
 
@@ -42,16 +41,58 @@ class _HomePageState extends State<HomePage> {
       drawer: MyDrawer(),
       body: Padding(
         padding: EdgeInsets.all(20),
-        child: CatalogModles.items !=null && CatalogModles.items.isNotEmpty ? ListView.builder(
-          itemCount: CatalogModles.items.length,
-          itemBuilder: (context, index) {
-            return ItemWidgets(
-              item: CatalogModles.items[index],
-            );
-          },
-        ) : Center(
-          child: CircularProgressIndicator(),
-        ) ,
+        child: CatalogModles.items != null && CatalogModles.items.isNotEmpty
+            // ListView.builder(
+            //   itemCount: CatalogModles.items.length,
+            //   itemBuilder: (context, index) {
+            //     return ItemWidgets(
+            //       item: CatalogModles.items[index],
+            //     );
+            //   },
+            // )
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 12,
+                  crossAxisSpacing: 12,
+                ),
+                itemBuilder: (context, index) {
+                  final item = CatalogModles.items[index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    color: Colors.black,
+                    child: GridTile(
+                      header: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                        ),
+                        child: Text(
+                          item.name,
+                          textScaleFactor: 1.2,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      footer: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                        ),
+                        child: Text(
+                          textScaleFactor: 1.7,
+                          item.price.toString(),
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      child: Image.network(item.image),
+                    ),
+                  );
+                },
+                itemCount: CatalogModles.items.length,
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
     );
   }
